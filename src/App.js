731c04fa75547593649a2aa7464d3c69d660f4f6
Richárd Nagy <big.ricsoo@gmail.com>
2022-03-18 14:08:31 +0100

    Replace element rendering with a loop

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import $ from "jquery";
 
 function renderImages() {
-	for (let i = 0; i < 5; i++) {
+	for (let i = 0; i < 6; i++) {
 		let img = new Image();
 		img.src = localStorage["image" + i];
 		$("#imagearea" + i).html(img);
@@ -24,7 +24,7 @@ function renderImages() {
 document.addEventListener(
 	"DOMContentLoaded",
 	function () {
-		for (let i = 0; i < 5; i++) {
+		for (let i = 0; i < 6; i++) {
 			document.querySelector("#image" + i).addEventListener("click", onclick, false);
 			function onclick() {
 				localStorage.removeItem("image" + i);
@@ -40,87 +40,29 @@ $(document).ready(function () {
 	renderImages();
 });
 
+function renderElements() {
+	let array = [];
+	for (let i = 0; i < 6; i++) {
+		array.push(
+			<div className="div">
+				<input id={"img" + i} className="hidden" type="file" />
+				<div id={"imagearea" + i} className="imagearea"></div>
+				<span>
+					<label htmlFor={"img" + i} className="add">
+						Add
+					</label>
+					<label className="remove" id={"image" + i}>
+						Remove
+					</label>
+				</span>
+			</div>
+		);
+	}
+	return array;
+}
+
 function App() {
-	return (
-		<div id="body">
-			<div class="div">
-				<input id="img0" class="hidden" type="file" />
-				<div id="imagearea0" class="imagearea"></div>
-				<span>
-					<label for="img0" class="add">
-						Add
-					</label>
-					<button class="remove" id="image0">
-						Remove
-					</button>
-				</span>
-			</div>
-			<div class="div">
-				<input id="img1" class="hidden" type="file" />
-				<div id="imagearea1" class="imagearea"></div>
-				<span>
-					<label for="img1" class="add">
-						Add
-					</label>
-					<button class="remove" id="image1">
-						Remove
-					</button>
-				</span>
-			</div>
-			<div class="div">
-				<input id="img2" class="hidden" type="file" />
-				<div id="imagearea2" class="imagearea"></div>
-				<span>
-					<label for="img2" class="add">
-						Add
-					</label>
-					<button class="remove" id="image2">
-						Remove
-					</button>
-				</span>
-			</div>
-			<div class="div">
-				<input id="img3" class="hidden" type="file" />
-				<div id="imagearea3" class="imagearea"></div>
-				<span>
-					<label for="img3" class="add">
-						Add
-					</label>
-					<button class="remove" id="image3">
-						Remove
-					</button>
-				</span>
-			</div>
-			<div class="div">
-				<input id="img4" class="hidden" type="file" />
-				<div id="imagearea4" class="imagearea"></div>
-				<span>
-					<label for="img4" class="add">
-						Add
-					</label>
-					<button class="remove" id="image4">
-						Remove
-					</button>
-				</span>
-			</div>
-			<div class="div">
-				<input id="img5" class="hidden" type="file" />
-				<div id="imagearea5" class="imagearea"></div>
-				<span>
-					<label for="img5" class="add">
-						Add
-					</label>
-					<button class="remove" id="image5">
-						Remove
-					</button>
-				</span>
-			</div>
-			<div>
-				<script src="query.min.js"></script>
-				<script src="popup.js" charset="utf-8"></script>
-			</div>
-		</div>
-	);
+	return <div id="body">{renderElements()}</div>;
 }
 
 export default App;
